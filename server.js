bf50a90ff0af48f699fcba2e9b85e265106a6d2e
ridsar <riday.sarkar@epita.fr>
2020-11-23 09:47:54 +0100

    newline added

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Bonjour. Vous avez bien containerisé l’application Node.js.');
+  res.send('Bonjour. Vous avez bien containerisé l’application Node.js.\n');
 });
 
 app.listen(PORT, HOST);
